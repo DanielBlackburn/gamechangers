3a828f8abc3ac60e142c8060dd8316fff08b976f
Daniel Blackburn <daniel@Daniels-MacBook-Pro.local>
2017-01-11 15:36:32 +0200

    new build

--- build/bundled/service-worker.js
+++ build/bundled/service-worker.js
@@ -29,7 +29,7 @@
 
 
 /* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["/index.html","3a4624391a2a3bcc0d01284f7faf1de5"],["/src/gc-app.html","a7cdc5f24b4979d86803c9fdc731afd1"]];
+var PrecacheConfig = [["/index.html","3a4624391a2a3bcc0d01284f7faf1de5"],["/src/gc-app.html","d29fa893555a3012a2b6e60a28d01670"]];
 /* eslint-enable quotes, comma-spacing */
 var CacheNamePrefix = 'sw-precache-v1--' + (self.registration ? self.registration.scope : '') + '-';
 
